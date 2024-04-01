88eb239ea31539126ec3ccddc077afd6f6b3c470
GabrielMorais2 <moraisgabriel274@gmail.com>
2024-04-01 00:53:01 -0300

    feat: add index.html and 404.html

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -10,8 +10,8 @@ resource "aws_s3_bucket" "static_site_bucket" {
   bucket = "static-site-${var.bucket_name}"
 
   website {
-    index_document = "index.html"
-    error_document = "error.html"
+    index_document = "/src/index.html"
+    error_document = "/src/404.html"
   }
 
   tags = {
